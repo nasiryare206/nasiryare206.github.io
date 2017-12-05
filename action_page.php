143e0051c77289ae059826763a17f307fdbd04c7
nasiryare206 <nasiryare206@gmail.com>
2017-12-05 02:43:47 -0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -4,16 +4,21 @@
 <h2> Just another header.</h2>
 
 <?php
-
    
   $dropdown = $_POST['dropdown'];
 	
   
-  if (empty($dropdown($_POST['selectcampus']))) {
+//    if (empty($dropdown($_POST['']))) {
+// 		echo "<p style = 'color: red;'class='error'>Please select a campus</p>";
+// 		//$okay = FALSE;
+// 	} else {
+// 		echo "<h4>https://nasiryare206.github.io/form.html</h4>";
+// 	} 
+ 
+	if ($dropdown = "selectcampus") {
 		echo "<p style = 'color: red;'class='error'>Please select a campus</p>";
 		//$okay = FALSE;
 	} else {
 		echo "<h4>https://nasiryare206.github.io/form.html</h4>";
-	}
-  
+	} 
 ?>
