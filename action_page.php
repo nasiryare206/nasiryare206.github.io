cecd642f960a4f0767e9cb2de6b95be1d1c4a85e
nasiryare206 <nasiryare206@gmail.com>
2017-12-05 04:49:15 -0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,18 +3,9 @@
 
 <h2> Just another header.</h2>
 <?php
-  //$submit = $_POST['submit'];
+ 
  $college = $_POST['college'];
 
 echo "Campus: $college";
 
-
- 
-// 	if ($dropdown = "selectcampus" && $submit = true) {
-// 		echo "<p style = 'color: red;'class='error'>Please select a campus</p>";
-// 		//$okay = FALSE;
-// 	} else (){
-// 		echo "<h4> https://nasiryare206.github.io/Roomitup/</h4>";
-// 	} 
-//    }
 ?>
