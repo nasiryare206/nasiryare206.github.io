f4c1f222487ce1787ef0808b4e71b545c87bd715
nasiryare206 <nasiryare206@gmail.com>
2017-12-05 02:25:55 -0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,1 +1,18 @@
+<h1> Please fill out the form</h1>
 
+
+<h2> Just another header.</h2>
+
+<?php
+   
+  $dropdown = $_POST['dropdown'];
+	
+  
+  if (($_POST['selectcampus'])) {
+		echo "<p style = 'color: red;'class='error'>Please select a campus</p>";
+		$okay = FALSE;
+	} else {
+		echo "<h4>form.html</h4>";
+	}
+  
+?>
